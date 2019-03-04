8da2152c07a0e78a2c3b9d9b7c2d34908cc2cd8a
favorable-vector-1743 <epzads@gmail.com>
2019-03-04 19:54:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,21 +1,13 @@
 
 def my_each (array)# put argument(s) here
-#def my_each(array)
 
-  #i = 0 
 
-  #while i < array.length
 
 array.each do |x|
     yield x 
-  #  i = i + 1
+
  end 
-#return array[i]
+
 end 
 
-#tas = ['arel', 'jon', 'logan', 'spencer']
 
-#my_each(tas) do |i|
- # print i
-#end
-  
